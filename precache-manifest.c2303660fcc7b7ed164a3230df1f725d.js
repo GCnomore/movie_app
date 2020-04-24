26dd2b0005706606d6e3ecae4267f5b269213734
GCnomore <63283305+GCnomore@users.noreply.github.com>
2020-04-23 20:56:02 -0700

    Updates

---
+++ precache-manifest.c2303660fcc7b7ed164a3230df1f725d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ddfa7c68b83788fbf5d949aa4a38172",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d03449cbbb876a2e4503",
+    "url": "/movie_app/static/css/main.44ab9c62.chunk.css"
+  },
+  {
+    "revision": "1936c2164ac8107b290f",
+    "url": "/movie_app/static/js/2.725c019e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.725c019e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03449cbbb876a2e4503",
+    "url": "/movie_app/static/js/main.e7ba5f1c.chunk.js"
+  },
+  {
+    "revision": "4dcb005dec2f66fe5231",
+    "url": "/movie_app/static/js/runtime-main.615d8c0e.js"
+  }
+]);
